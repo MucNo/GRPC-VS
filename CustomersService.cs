ce40467e81701e7bddc5732fc818731fb880910c
MucNo <khtn04hc@yahoo.com>
2020-04-17 16:24:31 +0700

    Add files via upload

---
+++ CustomersService.cs
@@ -0,0 +1,93 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            if (request.UserID == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+
+            }
+            else if (request.UserID == 2)
+            {
+                output.FirstName = "Jan";
+                output.LastName = "Doe";
+
+            }
+            else if (request.UserID == 3)
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomerRequest(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Tim",
+                    LastName = "Corye",
+                    EmailAdress = "test@gmail.com",
+                    Age = 21,
+                    IsAlive = true
+                
+                },
+
+                 new CustomerModel
+                {
+                    FirstName = "Ruby",
+                    LastName = "Nha khang",
+                    EmailAdress = "Ruby@gmail.com",
+                    Age = 21,
+                    IsAlive = true
+
+                },
+
+                  new CustomerModel
+                {
+                    FirstName = "Tom",
+                    LastName = "Jerry",
+                    EmailAdress = "TomJerry@gmail.com",
+                    Age = 21,
+                    IsAlive = true
+
+                },
+            };
+
+            foreach (var cust in customers)
+            {
+                await responseStream.WriteAsync(cust);
+            
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ PhoneService.cs
@@ -0,0 +1,84 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace GrpcServer.Services
+{
+ 
+    public class PhoneService : Phone.PhoneBase
+    {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;database=spam;username=root;password=NhaKhang0103");
+
+
+        private readonly ILogger<PhoneService> _logger;
+
+        public PhoneService(ILogger<PhoneService> logger)
+        {
+            logger = _logger;
+        }
+
+
+        public override Task<PhoneReply> GetPhoneNumber(PhoneRequest request, ServerCallContext context)
+        {
+            PhoneReply output = new PhoneReply();
+
+            if (request.ID == 1)
+            { 
+            
+            }
+            //viet code lay data sql o
+
+            return Task.FromResult(output);
+        }
+
+
+        public override async Task GetNewPhoneNumber(NewPhoneRequest request, IServerStreamWriter<PhoneReply> responseStream, ServerCallContext context)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+
+                String insertQuery = @"Select * from phone limit 10";
+                MySqlCommand command = new MySqlCommand(insertQuery, connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                String PhoneNumbers = "";
+
+                PhoneReply phoneReply = new PhoneReply();
+                while (reader.Read())
+                {
+                    String phone = $"{reader.GetString("number")} ";
+                    if (PhoneNumbers == "") PhoneNumbers = phone;
+                    else PhoneNumbers += phone;
+
+                }
+                phoneReply.Number = PhoneNumbers;
+                await responseStream.WriteAsync(phoneReply);
+
+            }
+            catch (Exception error)
+            {
+                String eror = error.Message;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+
+            }
+        }
+    }
+}
